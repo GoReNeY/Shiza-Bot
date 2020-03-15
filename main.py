3f551eff888cc540c573b47736a111d37c0ebfd0
GoReNeY <57534011+GoReNeY@users.noreply.github.com>
2020-03-15 19:22:39 +0200

    hotfix
    added check

--- main.py
+++ main.py
@@ -117,6 +117,8 @@ async def habr_start(ctx):
         pages = soup.find("ul", class_="toggle-menu toggle-menu_pagination").find_all("a")
         pages_list = [i.get("href") for i in pages]
         for page in pages_list:
+            if not page or page == None:
+                pass
             page_soup = BeautifulSoup(urllib.request.urlopen(main_url + page), features="html.parser")
             articles.extend(page_soup.find("ul", class_="content-list content-list_posts shortcuts_items").find_all("a", class_="post__title_link"))
         for post in articles[::-1]:
